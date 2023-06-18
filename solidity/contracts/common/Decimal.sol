b25eee6adfa69f8903b0f155940c4eb2565a13f4
flaviusburca <flaviusburca@users.noreply.github.com>
2023-06-18 10:38:52 +0300

    Moving contracts to sether-monorepo

--- solidity/contracts/common/Decimal.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.0;
-
-library Decimal {
-    // unit is used for decimals, e.g. 0.123456
-    function unit() internal pure returns (uint256) {
-        return 1e18;
-    }
-}
